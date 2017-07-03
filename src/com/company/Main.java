df79bbd4c206438b2cba61c231f536d1ea1862e9
DefeNder93 <alexdefender93@gmail.com>
2017-07-03 18:58:13 +0300

    desk description

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
     }
 }
+//   \  |   X   |  X
+//  ____|_______|____
+//      |       |
+//   X  |   \   |  X
+//  ____|_______|____
+//      |       |
+//   X  |   X   |  \
